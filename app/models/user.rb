794ddb3821cc3d8144681bcacf3caa3709e07071
frankNowinski <nowinski.frank@gmail.com>
2015-12-26 06:01:11 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ActiveRecord::Base
 
   def self.complete_transaction(session, params)
     user = self.find(session[:id])
-    if params[:post][:withdrawal].empty?
-      user.update(balance: (user.balance += params[:post][:deposit].to_i))
-    else
+    user.balance ||= 0
+
+    if !params[:post][:withdrawal].empty?
       user.update(balance: (user.balance -= params[:post][:withdrawal].to_i))
+    else
+      user.update(balance: (user.balance += params[:post][:deposit].to_i))
     end
   end
 end
