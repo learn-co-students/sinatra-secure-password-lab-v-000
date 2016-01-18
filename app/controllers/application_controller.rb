a5c72ec3583f179dd9898106f0cf392598f26320
dannyd4315 <dannyd4315@gmail.com>
2016-01-18 18:14:57 +1100

    Added Balance to users with Deposit and Withdrawal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -17,7 +18,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
+    user = User.new(:username => params[:username], :password => params[:password], :balance => 0)
     if user.save
       redirect '/login'
     else
@@ -34,17 +35,26 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:id] = user.id
-      redirect '/success'
+      redirect '/account'
     else
       redirect '/failure'
     end    
   end
 
-  get "/success" do
+  get "/account" do
     if logged_in?
-      erb :sucess
+      @user = current_user
+      erb :account
     else
       redirect "/login"
+    end
+  end
+
+  post "/account" do
+    if !withdrawal_deposit_approval(params)
+      erb :account_failure
+    else
+      redirect "/account"
     end
   end
 
@@ -64,6 +74,21 @@ class ApplicationController < Sinatra::Base
 
     def current_user
       User.find(session[:id])
+    end
+
+    def withdrawal_deposit_approval(params)
+      withdrawal = params[:withdrawal_amount]
+      deposit = params[:deposit_amount]
+      @user = current_user
+      if deposit.to_i > 0
+        @user.balance += deposit.to_i
+        @user.save
+      elsif  @user.balance >= withdrawal.to_i
+        @user.balance -= withdrawal.to_i
+        @user.save 
+      else
+        false  
+      end    
     end
   end
 
