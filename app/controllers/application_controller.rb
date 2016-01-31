0e10ab198c8d44e9112b48197a7a29baf5ed4e7e
coltonstaab1 <colton.staab@gmail.com>
2016-01-31 05:24:29 +0000

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,59 +1,132 @@
 require "./config/environment"
+
 require "./app/models/user"
+
 class ApplicationController < Sinatra::Base
 
+
   configure do
+
     set :views, "app/views"
+
     enable :sessions
+
     set :session_secret, "password_security"
+
   end
+
 
   get "/" do
+
     erb :index
+
   end
+
 
   get "/signup" do
+
     erb :signup
+
   end
 
+
   post "/signup" do
-    #your code here!
+
+    if params[:username] == "" || params[:password] == ""
+
+      redirect '/failure'
+
+    else
+
+      redirect '/login'
+
+    end
+
+
   end
+
+
+  get '/account' do
+
+    erb :account
+
+  end
+
 
 
   get "/login" do
+
     erb :login
+
   end
+
 
   post "/login" do
-    #your code here!
+
+    @user =  User.find_by(:username => params[:username])
+
+   if params[:username] == "" || params[:password] == ""
+
+      redirect '/failure'
+
+    else
+
+      session[:id] = @user.id
+
+      redirect '/account'
+
+    end
+
   end
+
 
   get "/success" do
+
     if logged_in?
+
       erb :success
+
     else
+
       redirect "/login"
+
     end
+
   end
+
 
   get "/failure" do
+
     erb :failure
+
   end
+
 
   get "/logout" do
+
     session.clear
+
     redirect "/"
+
   end
+
 
   helpers do
+
     def logged_in?
-      !!session[:id]
+
+      !!session[:user_id]
+
     end
 
+
     def current_user
-      User.find(session[:id])
+
+      User.find(session[:user_id])
+
     end
+
   end
+
 
 end
