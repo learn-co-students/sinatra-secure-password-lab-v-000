179bd8047633a1c22daa25b7bf753996e8526c2f
davdkm <davd.km@gmail.com>
2016-02-24 21:59:14 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,12 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here!
+    user = User.new(username: params[:username], password: params[:password], balance: 0)
+    if user.save && !user.username.empty?
+      redirect to "/login"
+    else
+      redirect to "/failure"
+    end
   end
 
 
@@ -27,11 +33,28 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     #your code here!
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:id] = user.id
+      redirect to "/account"
+    else
+      redirect to "/failure"
+    end
   end
 
+=begin replaced with /account
   get "/success" do
     if logged_in?
-      erb :success
+      erb :account
+    else
+      redirect "/login"
+    end
+  end
+=end
+
+  get "/account" do
+    if logged_in?
+      erb :account
     else
       redirect "/login"
     end
@@ -44,6 +67,20 @@ class ApplicationController < Sinatra::Base
   get "/logout" do
     session.clear
     redirect "/"
+  end
+
+  # deposits or withdraws from balance
+  post "/action" do
+    user = User.find(session[:id])
+    if params[:transaction] == "deposit"
+      user.balance += params[:amount].to_f
+    elsif user.balance > params[:amount].to_f && params[:transaction] == "withdraw"
+      user.balance -= params[:amount].to_f
+    else
+      redirect to '/failure'
+    end
+    user.save
+    redirect to '/account'
   end
 
   helpers do
