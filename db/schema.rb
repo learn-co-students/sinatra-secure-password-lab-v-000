3ff53d62c2ddb22fb0622345de1910e3863de410
thegands <thegands@gmail.com>
2016-03-05 22:14:37 -0700

    Completed Bonus

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306013853) do
+ActiveRecord::Schema.define(version: 20160306043833) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.float  "balance"
   end
 
 end
