1fa77d8557018ef36cd4e0be0a98a9586e3bf16c
the-widget <brennenawana108ny@gmail.com>
2016-03-17 12:01:25 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params["password"])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/account"
     else
