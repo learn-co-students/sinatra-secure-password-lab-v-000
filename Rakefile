a2e2e5d8c9ada93d09bb3ce47ec1ec2afdedc287
smithWEBtek <brad@smithwebtek.com>
2016-04-05 22:57:06 -0400

    passed tests, next is banking functions

--- Rakefile
+++ Rakefile
@@ -1,2 +1,8 @@
 require "./config/environment"
 require "sinatra/activerecord/rake"
+require 'pry'
+
+desc "gimme a console please"
+task :console do
+  Pry.start
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,55 +1,70 @@
-require "./config/environment"
-require "./app/models/user"
+require './config/environment'
+require './app/models/user'
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :views, "app/views"
+    set :views, 'app/views'
     enable :sessions
-    set :session_secret, "password_security"
+    set :session_secret, 'password_security'
   end
 
-  get "/" do
+  get '/' do
     erb :index
   end
 
-  get "/signup" do
+  get '/signup' do
     erb :signup
   end
 
-  post "/signup" do
-    #your code here
-
-  end
-
-  get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
-  end
-
-
-  get "/login" do
-    erb :login
-  end
-
-  post "/login" do
-    ##your code here
-  end
-
-  get "/success" do
-    if logged_in?
-      erb :success
+  post '/signup' do
+    user = User.new(:username => params[:username], :password => params[:password])
+    if 
+      params[:username] == ""
+      redirect '/failure'
+    elsif 
+      !user.save
+      redirect '/failure'
     else
-      redirect "/login"
+      user.save
+      redirect '/login'
     end
   end
 
-  get "/failure" do
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      redirect '/failure'
+    end
+  end
+
+  get '/success' do
+     if logged_in?
+       erb :success
+     else
+       redirect '/failure'
+     end
+   end
+
+  get '/account' do
+    # @user = current_user
+    erb :account
+  end
+
+  get '/failure' do
     erb :failure
   end
 
-  get "/logout" do
+  get '/logout' do
     session.clear
-    redirect "/"
+    redirect '/'
   end
 
   helpers do
@@ -61,5 +76,4 @@ class ApplicationController < Sinatra::Base
       User.find(session[:user_id])
     end
   end
-
 end
