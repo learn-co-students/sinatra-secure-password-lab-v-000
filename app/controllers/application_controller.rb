0cf06386b3bf7fd863f0d99e381bbab0adc9ee5f
Vannak Chhay <vannak.chhay1@gmail.com>
2016-04-25 20:15:12 -0400

    fixed checking username and password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(username: params[:username], password: params[:password])
-    if user.save
-      redirect "/account"
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
     else
-      redirect "/failure"
+      User.create(username: params[:username], password: params[:password])
+      redirect '/login'
     end
   end
 
