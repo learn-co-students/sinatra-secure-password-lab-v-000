20860c370a53609895f5a33e7c492a5de6c68dee
PeteAJ <peter.jacobson@gmail.com>
2016-05-06 14:23:17 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,31 +17,33 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-     if User.create(:username => params[:username], :password => params[:password])
-      @user.save
-      redirect "/login"
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
     else
-     redirect "/failure"
+      User.create(username: params[:username], password: params[:password])
+      redirect '/login'
     end
+
   end
 
   get '/account' do
-    @user = User.find_by_id(session[:user_id])
+    @user = User.find(session[:user_id])
     erb :account
   end
+
 
   get "/login" do
     erb :login
   end
 
   post "/login" do
-        user = User.find_by(:username => params[:username])
-      if user && user.authenticate(params[:password])
-        session[:id] = user.id
-        redirect "/account"
-      else
-        redirect "failure"
-      end
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/account"
+    else
+      redirect to "/failure"
+    end
   end
 
   get "/success" do
--- db/migrate/20160506174356_create_users.rb
+++ db/migrate/20160506174356_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :password
+      t.string :username
+      t.string :password_digest
     end
   end
 end
