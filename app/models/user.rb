37e0874ca7b38657d7fd9986929bc68bdb0cb64f
ldgarber <leah.garber88@gmail.com>
2016-05-07 09:46:47 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :username, :password, presence: true
+  has_secure_password 
+
 end
---
+++ db/migrate/20160507132422_add_password_digest_to_users.rb
@@ -0,0 +1,7 @@
+class AddPasswordDigestToUsers < ActiveRecord::Migration
+  def change
+    change_table :users do |t|
+      t.rename :password, :password_digest
+    end
+  end
+end
