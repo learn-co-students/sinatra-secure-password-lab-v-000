d0362255f61f934865b8222eb7d55941fb370717
miclp <michaelpeimani@gmail.com>
2016-05-28 15:12:02 -0700

    Add bcrypt encryption to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    if params[:username] != "" && params[:password] != ""
+      user = User.new(username: params[:username], password: params[:password])
+      user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
   end
 
   get '/account' do
@@ -32,7 +37,17 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+
+    if params[:username] != "" && params[:password] != ""
+      # go to the account page
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/account"
+      end
+    else
+      redirect "/failure"
+    end
   end
 
   get "/success" do
