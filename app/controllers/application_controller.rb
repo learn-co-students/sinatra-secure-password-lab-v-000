529c598ffef0d18b18afa84441a2123c7cf8507f
Savannah Scott <sascott624@gmail.com>
2016-06-16 15:11:55 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    user = User.new(username: params[:username], password: params[:password])
+    if user.save && params[:username] != ""
+      redirect to '/login'
+    else
+      redirect to '/failure'
+    end
   end
 
   get '/account' do
@@ -33,6 +37,13 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/account'
+    else
+      redirect to '/failure'
+    end
   end
 
   get "/success" do
---
+++ db/migrate/20160616144751_edit_users.rb
@@ -0,0 +1,6 @@
+class EditUsers < ActiveRecord::Migration
+  def change
+    remove_column :users, :password
+    add_column :users, :password_digest, :string
+  end
+end
