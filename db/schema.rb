7aafab313ccf68154f631c47f451a918605cd5b3
ndaniele <n.daniele@gmail.com>
2016-07-06 13:28:21 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706163019) do
+ActiveRecord::Schema.define(version: 20160706162509) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
