df7422113afa82b752357d76a7fa35a9f00b1e6d
wook kim <wooke2e@gmail.com>
2016-07-07 19:02:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/failure'
+    else
+      User.new(:username => params[:username], :password => params[:password])
+      redirect to '/login'
+    end
   end
 
   get '/account' do
@@ -32,14 +36,20 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      redirect to '/failure'
+    end
   end
 
   get "/success" do
     if logged_in?
       erb :success
     else
-      redirect "/login"
+      redirect to '/login'
     end
   end
 
