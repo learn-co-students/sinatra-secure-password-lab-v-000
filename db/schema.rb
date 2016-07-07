06dd570f7e46740f23f6a488db3f3b0ec3a922d4
wook kim <wooke2e@gmail.com>
2016-07-07 20:19:44 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707170935) do
+ActiveRecord::Schema.define(version: 20160707201741) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
