c55d3ae3942d9dc9332033c1ce142c326eecf11d
auranbuckles <auran59@gmail.com>
2016-08-12 15:56:32 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
-    if user.save
+    if user.save && user.username && user.username != ""
         redirect "/login"
     else
         redirect "/failure"
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect "/success"
+        redirect "/account"
     else
         redirect "/failure"
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20160812181236) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
