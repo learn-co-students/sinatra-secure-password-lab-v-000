9483c051ec8796fafb4effa32ff9eec7a7cb61d7
Jonathan Stone <jon_p_stone@yahoo.com>
2016-08-21 01:54:31 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20160820101209) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
