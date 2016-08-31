4a945cf02a7daac7a4094eeb578d66dff4a4a37d
ThisIsTyDell <milleb94@gmail.com>
2016-08-31 15:59:48 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,23 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      user = User.create(username: params[:username], password: params[:password])
+      redirect '/login'
+    end
   end
 
   get '/account' do
     @user = User.find(session[:user_id])
+    erb :account
+  end
+
+  post '/account' do
+    @withdrawl_amount = params[:withdraw].to_f
+    @deposit_amount = params[:deposit].to_f
+    update_balance
     erb :account
   end
 
@@ -32,7 +43,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/account"
+    else
+      redirect to "/failure"
+    end
   end
 
   get "/success" do
@@ -59,6 +76,19 @@ class ApplicationController < Sinatra::Base
 
     def current_user
       User.find(session[:user_id])
+    end
+
+    def update_balance
+      @user = User.find(session[:user_id])
+      if @withdrawl_amount > 0 && @withdrawl_amount <= @user.balance.to_f
+        @user.balance = @user.balance.to_f - @withdrawl_amount
+        @user.save
+      elsif @deposit_amount.to_f > 0
+        @user.balance = @user.balance.to_f + @deposit_amount
+        @user.save
+      else
+        nil
+      end
     end
   end
 
