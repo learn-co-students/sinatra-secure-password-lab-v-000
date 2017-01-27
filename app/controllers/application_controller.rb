37850d2debcda8496cfd5ddcde764dfd5182acdf
Callie Brazil <cmabrazil@gmail.com>
2017-01-27 00:50:12 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,9 +35,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username])
-		if user && user.authenticate(params[:password])
-			session[:user_id] = user.id
+    @user = User.find_by(username: params[:username])
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id
 			redirect "/account"
 		else
 			redirect "/failure"
