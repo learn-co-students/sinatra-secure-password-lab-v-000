750c2cae17793e0a3b7c3d127221a1310312a9e8
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-03-14 10:07:03 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,65 +1,84 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :views, "app/views"
-    enable :sessions
-    set :session_secret, "password_security"
-  end
-
-  get "/" do
-    erb :index
-  end
-
-  get "/signup" do
-    erb :signup
-  end
-
-  post "/signup" do
-    #your code here
-
-  end
-
-  get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
-  end
-
-
-  get "/login" do
-    erb :login
-  end
-
-  post "/login" do
-    ##your code here
-  end
-
-  get "/success" do
-    if logged_in?
-      erb :success
-    else
-      redirect "/login"
-    end
-  end
-
-  get "/failure" do
-    erb :failure
-  end
-
-  get "/logout" do
-    session.clear
-    redirect "/"
-  end
-
-  helpers do
-    def logged_in?
-      !!session[:user_id]
+    configure do
+        set :views, "app/views"
+        enable :sessions
+        set :session_secret, "password_security"
     end
 
-    def current_user
-      User.find(session[:user_id])
+    get "/" do
+        erb :index
     end
-  end
+
+    get "/signup" do
+        erb :signup
+    end
+
+    post "/signup" do
+
+        if params[:username] != '' && params[:password] != ''
+            
+            @user = User.find_or_create_by(username: params[:username], password_digest: params[:password])
+            redirect '/login'
+        else
+            redirect '/failure'
+        end
+    end
+
+    get '/account' do
+        @user = User.find(session[:user_id])
+        erb :account
+    end
+
+
+    get "/login" do
+        erb :login
+    end
+
+    post "/login" do
+        if params[:username] != '' && params[:password] != ''
+            user = User.find_by(username: params[:username])
+        end
+#binding.pry
+        if !!(user && user.authenticate(params[:password]))
+
+            session[:user_id] = user.id
+            redirect '/success'
+        else
+            redirect '/failure'
+        end
+    end
+
+    get "/success" do
+#        
+        if logged_in?
+#            binding.pry
+            redirect '/account'
+        else
+            redirect "/login"
+        end
+    end
+
+    get "/failure" do
+        erb :failure
+    end
+
+    get "/logout" do
+        session.clear
+        redirect "/"
+    end
+
+    helpers do
+        def logged_in?
+            !!session[:user_id]
+        end
+
+        def current_user
+            User.find(session[:user_id])
+        end
+    end
 
 end
