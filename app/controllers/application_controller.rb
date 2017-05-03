3eefde2dd0d133d891c525889a828c974eff755f
Zenia Villa <zeniavilla@hotmail.com>
2017-05-03 14:13:04 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -17,7 +18,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(username: params[:username], password: params[:password])
+    user = User.new(username: params[:username], password: params[:password], balance: 0)
     if user.save
       redirect "/login"
     else
@@ -26,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find(session[:user_id])
+    @user = current_user
     erb :account
   end
 
@@ -57,6 +58,20 @@ class ApplicationController < Sinatra::Base
     erb :failure
   end
 
+  get "/deposit" do
+    erb :deposit
+  end
+
+  post "/deposit" do
+    @user = current_user
+    @user.balance += params[:deposit].to_f
+    erb :account
+  end
+
+  get "/withdraw" do
+    erb :withdraw
+  end
+
   get "/logout" do
     session.clear
     redirect "/"
@@ -69,6 +84,14 @@ class ApplicationController < Sinatra::Base
 
     def current_user
       User.find(session[:user_id])
+    end
+
+    def withdraw(amount)
+      user.balance -= amount
+    end
+    
+    def deposit(amount)
+      current_user.balance += amount.to_f
     end
   end
 
