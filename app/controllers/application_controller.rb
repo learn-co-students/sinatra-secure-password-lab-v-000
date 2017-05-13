f1235f66d5ad9ad9775b8ae011ca149827f521e3
Jdville03 <jarrel.delottinville@gmail.com>
2017-05-13 15:33:34 -0400

    update soft tabs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,8 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(username: params[:username], password: params[:password])
-		if params[:username].match(/\S/) && user.save
-    	redirect "/login"
+    if params[:username].match(/\S/) && user.save
+      redirect "/login"
     else
       redirect "/failure"
     end
