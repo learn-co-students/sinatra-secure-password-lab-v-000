59b089e2f45850840ae1d94bca88195a04c72c6d
Niki Lee <ew1415048nk@gmail.com>
2017-06-11 02:06:47 +0900

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170610162331) do
 
   create_table "users", force: :cascade do |t|
     t.string  "username"
-    t.string  "password"
+    t.string  "password_digest"
     t.decimal "balance"
   end
 
