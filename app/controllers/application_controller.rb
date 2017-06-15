09826e3c53cc46c6495b387015428f852cc680f7
Terry Blue <terryblue@comcast.net>
2017-06-15 01:11:25 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+      if params[:username].size > 0
+        user = User.new(:username => params[:username], :password => params[:password])
+          if user.save
+            redirect "/login"
+          else
+            redirect "/failure"
+          end
+      else
+        redirect "/failure"
+      end
   end
 
   get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
+    if logged_in?
+      @user = User.find(session[:user_id])
+      erb :account
+    else
+      redirect "/"
+    end
   end
 
 
@@ -32,11 +44,16 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       redirect "/account"
+    else
+       redirect "/failure"
+    end
   end
 
-  get "/success" do
-    if logged_in?
+  get "/success" do    if logged_in?
       erb :success
     else
       redirect "/login"
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,12 @@
 require 'bundler'
 Bundler.require
 
+require 'capybara/dsl'
+
 configure :development do
 	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
 end
+
 require_relative '../app/controllers/application_controller.rb'
-require_all 'app/models'
+
+require_all 'app'
