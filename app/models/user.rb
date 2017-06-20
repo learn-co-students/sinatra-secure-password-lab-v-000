9262476734b517fbc1da1ff20295bc19a119824a
Amanda George <github email address>
2017-06-20 18:55:57 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+
+  # validates_presence_of :username, :password
+
+  after_initialize :init
+
+  def init
+    self.balance ||= 0.00
+  end
+
 end
