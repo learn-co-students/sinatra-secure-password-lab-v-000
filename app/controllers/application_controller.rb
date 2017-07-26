553f3fa6287af7682431b275ed3b82c5d7fe56b2
Scott Wakeman <swakeman@ca.rr.com>
2017-07-26 16:32:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,9 @@ class ApplicationController < Sinatra::Base
     #your code here
     @user = User.new(username: params[:username], password: params[:password])
       if @user.save
-        redirect "/login"
+        redirect "/account"
       else
+
         redirect "/failure"
       end
   end
