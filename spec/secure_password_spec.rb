35e662a7876f7a8a372c25f1680f1d7d389e481b
ctab33 <ctab33@me.com>
2017-08-02 20:05:14 -0400

    Done.

--- spec/secure_password_spec.rb
+++ spec/secure_password_spec.rb
@@ -70,6 +70,7 @@ describe 'App' do
       fill_in "username", :with => "penelope"
       fill_in "password", :with => "puppies"
       click_button "Log In"
+
       expect(page.current_path).to eq('/account')
       expect(page.status_code).to eq(200)
       expect(page.body).to include("We are currently working on your account.")
