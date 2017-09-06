038f95cd569bec9605a8d83afb8215b2a88249ee
Alec Alba <alecgalba@gmail.com>
2017-09-06 18:13:26 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :username, :password_digest, presence: :true
+
+  def deposit(amount)
+    self.balance += amount
+    self.save
+    "Your deposit was a success! You are now $#{amount} wealthier!"
+  end
+
+  def withdrawal(amount)
+    if self.balance > amount
+      self.balance -= amount
+      self.save
+      "You have withdrawn $#{amount}. Your current balance is $#{self.amount} Don't spend it all at once!"
+    else
+      "You don't have enough monies! This transaction cannot be completed."
+    end
+  end
 end
