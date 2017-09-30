3848d894e1148c2d038fc7bb3e9c66c15e0eaaa0
Donna <donnamcla@gmail.com>
2017-09-30 14:28:14 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    if params[:username] == "" || params[:password] == ""
+    if params[:username].empty? || params[:password].empty?
       redirect "/failure"
     else
       User.create(:username => params[:username], :password => params[:password])
