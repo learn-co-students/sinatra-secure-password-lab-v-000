3d691ba67f9759c9000eaf0bf990730399d076a6
Richard Steinbach <steinbach.dick@gmail.com>
2017-11-15 15:26:16 -0800

    sinatra-secure-password-lab-v-000

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -18,6 +19,13 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
+    user = User.new(:username => params[:username], :password => params[:password], :balance => 0)
+
+    if user.save && user[:username].size > 0
+        redirect "/login"
+    else
+        redirect "/failure"
+    end
 
   end
 
@@ -33,11 +41,20 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        #binding.pry
+        redirect "/account"
+
+    else
+        redirect "/failure"
+    end
   end
 
-  get "/success" do
+  get "/account" do
     if logged_in?
-      erb :success
+      erb :account
     else
       redirect "/login"
     end
@@ -50,6 +67,24 @@ class ApplicationController < Sinatra::Base
   get "/logout" do
     session.clear
     redirect "/"
+  end
+
+  post "/deposit" do
+    user = current_user
+    user[:balance] += params[":deposit"].to_f
+    user.save
+    redirect "/account"
+  end
+
+  post "/withdrawl" do
+    user = current_user
+    if user[:balance] > params[":withdrawl"].to_f
+        user[:balance] -= params[":withdrawl"].to_f
+        user.save
+        redirect "/account"
+    else
+      redirect "/account"
+    end
   end
 
   helpers do
