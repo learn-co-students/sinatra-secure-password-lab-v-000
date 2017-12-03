e73d89f21d472ba1e44c089c1d1cefe65cb288af
Jacilyn Hayden <jacihayden@gmail.com>
2017-12-03 01:04:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,9 +35,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username])
+    @user = User.find_by(:username => params[:username])
 
-	  if user && user.authenticate(params[:password])
+	  if @user && @user.authenticate(params[:password])
 	    session[:user_id] = user.id
 	    redirect "/account"
 	  else
@@ -45,14 +45,13 @@ class ApplicationController < Sinatra::Base
 	  end
   end
 
-  # this route appears to be unnecessary
-  # get "/success" do
-  #   if logged_in?
-  #     redirect "/account"
-  #   else
-  #     redirect "/login"
-  #   end
-  # end
+  get "/success" do
+    if logged_in?
+      redirect "/account"
+    else
+      redirect "/login"
+    end
+  end
 
   get "/failure" do
     erb :failure
