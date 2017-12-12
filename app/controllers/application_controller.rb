f36591bbcb9cab1632552d3195439857b98b301f
Hunter Kleindl <hkleindl@gmail.com>
2017-12-12 19:04:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    # binding.pry
     user = User.new(username: params[:username], password: params[:password])
-
-    if user
+      # binding.pry
+    if !user.username.empty? && user.password
+      # user.save
       redirect '/login'
     else
       redirect '/failure'
@@ -38,7 +38,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/account'
+    else
+      redirect '/failure'
+    end
   end
 
   get "/success" do
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212154230) do
+ActiveRecord::Schema.define(version: 20171212175310) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.float  "balance"
+  end
 
 end
