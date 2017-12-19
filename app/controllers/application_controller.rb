939168d16f7b7239e2a03e585e2f07cc85be8b43
github username <github email address>
2017-12-19 12:22:56 +0200

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -17,7 +18,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
+    @user = User.new(username: params[:username], password: params[:password])
+    if @user[:username] == ""
+      redirect "/failure"
+    elsif @user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
 
   end
 
@@ -32,12 +40,18 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    @user = User.find_by(username: params[:username])
+  		if @user && @user.authenticate(params[:password])
+  			session[:user_id] = @user.id
+  			redirect "/account"
+  		else
+  			redirect "/failure"
+  		end
   end
 
   get "/success" do
     if logged_in?
-      erb :success
+      erb :account
     else
       redirect "/login"
     end
