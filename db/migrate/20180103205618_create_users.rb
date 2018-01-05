1765904d79730f2cd150f81fb6b11a002d40e0e1
Yujun Sung <yujunsung@gmail.com>
2018-01-05 04:48:46 +0000

    Done.

---
+++ db/migrate/20180103205618_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+
+    def up
+
+      create_table :users do |t|
+        t.string :username
+        t.string :password_digest
+        t.string :name
+        t.string :password
+      end
+
+    end
+
+    def down
+      drop_table :users
+    end
+
+end
