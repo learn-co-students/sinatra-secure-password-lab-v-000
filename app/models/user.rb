dc6dc15db17f1a54e0b9a777b4f748387421452d
Jacqueline Lee <anginblu@gmail.com>
2018-01-09 07:12:29 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :username, :password
-
+  # attr_accessor :balance
+  # def initialize(params)
+  #   @balance = 0
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@
 ActiveRecord::Schema.define(version: 20180109063854) do
 
   create_table "users", force: :cascade do |t|
-    t.string  "username"
-    t.string  "password_digest"
-    t.decimal "balance"
+    t.string "username"
+    t.string "password_digest"
+    t.float  "balance",         default: 0.0
   end
 
 end
