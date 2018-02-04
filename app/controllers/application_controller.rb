162381302325b0673c9a6377d35311b47b089f52
Parker Catalano <catalano.work@gmail.com>
2018-02-04 21:59:50 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
+
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -17,7 +19,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
+    user = User.new(:username => params[:username], :password => params[:password])
+
+		if user.save
+			 redirect "/login"
+	 else
+			 redirect "/failure"
+	 end
 
   end
 
@@ -32,7 +40,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(username: params["username"])
+
+    if user && user.authenticate(params["password"])
+      session[:user_id] = user.id
+      redirect '/account'
+    else
+      redirect '/failure'
+    end
   end
 
   get "/success" do
@@ -50,6 +65,18 @@ class ApplicationController < Sinatra::Base
   get "/logout" do
     session.clear
     redirect "/"
+  end
+
+  get '/deposit' do
+    erb :deposit
+  end
+
+  post '/depositing' do
+    @user = User.find(session[:user_id])
+    @user.deposit(params["deposit"])
+    @user.save
+
+    erb :success
   end
 
   helpers do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :username, :presence => true
+
+  after_initialize :init
+
+  def init
+    self.balance ||= 0.00
+  end
+
+  def deposit(amount)
+    balance + amount.to_i
+  end
+
+  def withdraw(amount)
+    balance - amount.to_i
+  end
+    
+  
 end
