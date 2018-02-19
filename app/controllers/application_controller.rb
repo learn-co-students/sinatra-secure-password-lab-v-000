16d14c50e989e6f65ff3da4195e318b4476b8506
Sally Zasloff <szasloff@gmail.com>
2018-02-19 22:18:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,6 @@ class ApplicationController < Sinatra::Base
       else
         redirect '/failure'
       end
-
   end
 
   get "/account" do
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,9 @@
 ActiveRecord::Schema.define(version: 20180219203256) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
+    t.string  "username"
+    t.string  "password_digest"
+    t.integer "balance"
   end
 
 end
