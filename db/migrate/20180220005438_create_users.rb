9c63ff768d9e4fe37065d738127f092af832bf71
Sally Zasloff <szasloff@gmail.com>
2018-02-20 01:01:35 +0000

    Done.

--- db/migrate/20180220005438_create_users.rb
+++ db/migrate/20180220005438_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      t.integer :balance, default: 0
+      t.integer :balance
     end
   end
 end
