b8fced420b85d220d30a65c33061c914d0c5c717
funemployeddad <funemployeddad@gmail.com>
2018-02-22 02:15:46 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,15 +68,6 @@ class ApplicationController < Sinatra::Base
       !!current_user
     end
 
-    # def login(username, password)
-    #     user = User.find_by(:username => username)
-    #   if user && user.authenticate(password)
-    #     session[:id] = user.id
-    #   else
-    #     redirect '/login'
-    #   end
-    # end
-    #
     def current_user
       @current_user ||= User.find(session[:id]) if session[:id]
     end
