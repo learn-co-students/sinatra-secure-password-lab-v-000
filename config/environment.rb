91e5cf6a95832b20469c879836a72c93ff74e7e1
jljardon <github email address>
2018-03-05 01:13:40 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,13 @@
 require 'bundler'
 Bundler.require
 
+
+ActiveRecord::Base.logger.level = :info
 configure :development do
 	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
 end
+
+
+
 require_relative '../app/controllers/application_controller.rb'
 require_all 'app/models'
