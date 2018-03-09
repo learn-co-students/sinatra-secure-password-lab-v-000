9d7aea4298cc32293be2b358a8fb3edf33d1dd9b
Blakeley Bond <burrick2003@yahoo.com>
2018-03-09 03:20:01 +0000

    Done.

--- db/migrate/20180309024529_create_users.rb
+++ db/migrate/20180309024529_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t| #from dog example in activerecord setup in sinatra
       t.string :username #had to change this like 10 times, in the future look in seeds
-      t.string :password
+      t.string :password_digest
       t.decimal :balance #http://edgeguides.rubyonrails.org/active_record_migrations.html for this
     end
 
