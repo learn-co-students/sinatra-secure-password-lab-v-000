15035b671cfbb2e21b815f01ebaf816b96c29f30
camneu37 <neuner.camille@gmail.com>
2018-03-22 22:07:21 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates_presence_of :username, :password
+
+  def deposit(amt)
+    balance = balance + amt
+  end
+
+  def withdrawl(amt)
+    balance = balance - amt
+  end
+
 end
