e62f4c5de32aeacc9d4238f123e1e55bc6e22eea
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-04-03 01:48:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,11 +18,11 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    binding.pry
+    # binding.pry
     if params[:username] == "" || params[:password] == ""
       redirect '/failure'
     else
-      user = User.new(username: params[:username], password: params[:password])
+      user = User.new(username: params[:username], password_digest: params[:password])
       redirect '/login'
     end
   end
@@ -39,6 +39,13 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session["user_id"] = user.id
+      redirect '/account'
+    else
+      redirect '/failure'
+    end
   end
 
   get "/failure" do
