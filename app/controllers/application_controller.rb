bb1313452e4efe7349d5d41a0ad3dce22ef2b71f
Jay Bednar <ironviralmedia@gmail.com>
2018-04-06 23:23:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,15 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-
+    if params[:username] != "" && params[:password] != ""
+      @user = User.new(username: params[:username], password: params[:password])
+      if @user.save
+        session[:user_id] = @user.id
+        redirect '/login'
+      end
+    else
+      redirect '/failure'
+    end
   end
 
   get '/account' do
@@ -33,6 +41,13 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      redirect '/failure'
+    end
   end
 
   get "/failure" do
