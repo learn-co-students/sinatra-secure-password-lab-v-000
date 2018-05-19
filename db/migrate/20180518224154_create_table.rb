86b7245ccf125c1b190c3a222246dcdc30f65caf
Zac Willmington <zac.willmington@gmail.com>
2018-05-19 00:46:29 +0000

    Done.

---
+++ db/migrate/20180518224154_create_table.rb
@@ -0,0 +1,9 @@
+class CreateTable < ActiveRecord::Migration
+  def change
+      create_table :users do |t|
+          t.string :username
+          t.string :password_digest
+          t.float :balance
+      end
+  end
+end
