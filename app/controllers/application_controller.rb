1e965fd3402452a0a5b7db3c33c6a8fbe4ee29f1
Jayon Kim <jpkim921@gmail.com>
2018-05-29 23:28:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,13 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
+    # binding.pry
+    user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect '/login'
+		else
+			redirect '/failure'
+		end
 
   end
 
