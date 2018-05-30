a18fdd1abfbb9ca9a5d6cc7fd2b01a610b16be34
Jayon Kim <jpkim921@gmail.com>
2018-05-30 01:50:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -18,13 +19,23 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
+    # user = User.new(:username => params[:username], :password => params[:password])
     # binding.pry
-    user = User.new(:username => params[:username], :password => params[:password])
-		if user.save
-			redirect '/login'
-		else
-			redirect '/failure'
-		end
+
+		# if user.username != "" && user.password != ""
+    #   user.save
+		# 	redirect '/login'
+		# else
+		# 	redirect '/failure'
+		# end
+
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      user = User.new(:username => params[:username], :password => params[:password])
+      user.save
+      redirect '/login'
+    end
 
   end
 
@@ -33,13 +44,21 @@ class ApplicationController < Sinatra::Base
     erb :account
   end
 
-
   get "/login" do
     erb :login
   end
 
   post "/login" do
     ##your code here
+    user = User.find_by(:username => params[:username])
+   
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/account"
+    else
+        redirect "/failure"
+    end
+    
   end
 
   get "/failure" do
