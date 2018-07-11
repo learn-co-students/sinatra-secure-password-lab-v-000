ca799a027515f4aabefc394b4c81e86a0a1aa4dc
osxgirl <osxgirl@mac.com>
2018-07-11 10:20:22 -0400

    //❤️

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'activerecord'
+
 gem 'sinatra-activerecord'
 gem 'rake'
 gem 'bcrypt'
---
+++ db/migrate/01_users.rb
@@ -0,0 +1,14 @@
+class Users < ActiveRecord::Migration[5.1]
+    
+        def change
+            create_table :users do |t|
+                t.string :username
+                t.string :password_digest
+                end
+        end
+
+        def down
+            drop_table :users
+        end
+        
+end
