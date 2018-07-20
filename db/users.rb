0b85a4a35fe46dc3a240f2f1f851cdadeccc03de
Judy McGoldrick <jjeanius@verizon.net>
2018-07-20 03:30:02 +0000

    Done.

---
+++ db/users.rb
@@ -0,0 +1,13 @@
+class Users < ActiveRecord migration[5.1]
+  def up
+    create_table :users do|t|
+      t.string :username
+      t.string :password
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+
+end
