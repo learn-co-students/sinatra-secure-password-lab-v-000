58ef939f9b279b415abd68f2f04b0dfddf983bcc
Brett Weisberg <weisberg.brett@gmail.com>
2018-07-24 02:35:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,9 +19,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-
+  #binding.pry
     user = User.new(:username => params[:username], :password => params[:password])
-    binding.pry
+
   if user.save
       redirect '/login'
   else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+#require 'bcrypt'
+
 class User < ActiveRecord::Base
+  has_secure_password
+  # users.password_hash in the database is a :string
+  #include BCrypt
+
+#  def password
+#    @password ||= Password.new(password_hash)
+#  end
+
+#  def password=(new_password)
+  #  binding.pry
+#    @password = Password.create(new_password)
+#    self.password_digest = @password
+#  end
 end
