289ccde1257091efe7411f40684ab10206e5a10b
Daniel Hernandez <elreymcd@yahoo.com>
2018-07-28 23:05:23 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-    if user.save
-      redirect "/login"
-    else
+    if params[:username].size > 0
+      user = User.new(:username => params[:username], :password => params[:password], :balance => 0)
+      if user.save
+        redirect "/login"
+      else
+        redirect "/failure"
+      end
+    else 
       redirect "/failure"
     end
   end
@@ -39,13 +43,43 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
+      @user = user
       session[:user_id] = user.id
-      redirect "/success"
+      redirect "/account"
     else
       redirect "/failure"
     end
   end
   
+  get "/deposit" do
+    @user = User.find(session[:user_id])
+    erb :deposit
+  end
+
+  get "/withdrawal" do    
+    @user = User.find_by(:id => session[:user_id])
+    erb :withdrawal
+  end
+
+  post "/deposit" do
+    @user = User.find(session[:user_id])
+    @user[:balance]+= params[:deposit].to_f
+    User.update(session[:user_id], :balance => @user[:balance])
+    redirect "/account"
+  end
+
+  post "/withdrawal" do    
+    @user = User.find(session[:user_id])
+    if @user[:balance] > params[:withdrawal].to_f
+      @user[:balance]-= params[:withdrawal].to_f
+      User.update(session[:user_id], :balance => @user[:balance])
+      redirect "/account" 
+    else
+       @message = "You have insufficient funds to cover that withdrawal"
+       puts @message
+       redirect "/account"
+    end
+  end
 
   get "/failure" do
     erb :failure
