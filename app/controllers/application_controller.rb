e260ee8025521654a35bfcc63a6d4e94b935d7ec
Lorena <lorena.giddens@outlook.com>
2018-08-15 17:08:41 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,18 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
+    # display the failure page if no username is given
+    if params[:username].empty?
+      redirect "/failure"
+    end
 
+    user = User.new(:username => params[:username], :password => params[:password])
+
+    if user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
   end
 
   get '/account' do
@@ -32,7 +42,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
   end
 
   get "/failure" do
