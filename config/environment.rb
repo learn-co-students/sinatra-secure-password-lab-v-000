e03b764fe351b398e69e61cbd48577c7d67bbe23
Tao Liu <taoliudesign@gmail.com>
2018-10-16 03:57:53 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,4 @@ configure :development do
 	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
 end
 # require_relative '../app/controllers/application_controller.rb'
-require_all 'app'
+require_all '../app'
