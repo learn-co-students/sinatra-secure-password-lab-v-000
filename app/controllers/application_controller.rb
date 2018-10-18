0d7b11b40a4e4cb6c052e8398aeeb37d969e383e
Adam Barton <adam.barton333@gmail.com>
2018-10-18 22:44:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,14 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    @user = User.find_by(:username => params[:username])
+
+		 if @user && @user.authenticate(params[:password])
+			 session[:user_id] = @user.id
+			 redirect "/account"
+     else
+       redirect "/failure"
+     end
   end
 
   get "/failure" do
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20181018181258) do
 
   create_table "users", force: :cascade do |t|
     t.string  "username"
-    t.string  "password"
+    t.string  "password_digest"
     t.decimal "balance"
   end
 
