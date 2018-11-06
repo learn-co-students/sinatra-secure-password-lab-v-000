252336d85d0850b90b88861d77a18750f701f1a9
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-11-06 21:28:27 +0000

    Done.

---
+++ db/migrate/20181106210642_users.rb
@@ -0,0 +1,9 @@
+class Users < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.decimal :balance, :default => 0
+    end
+  end
+end
