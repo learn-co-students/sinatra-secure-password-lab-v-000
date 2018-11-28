cffaa4cd2d5d140c1532eff025b9fd6fab9d1420
Melissa Real <melissa.real@hotmail.com>
2018-11-28 15:04:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
-		if user.username != "" && user.save
+		if !user.username.empty? && user.save
     	redirect "/login"
   	else
     	redirect "/failure"
