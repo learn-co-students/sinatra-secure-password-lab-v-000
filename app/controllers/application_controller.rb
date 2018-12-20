588e2cae209f784da7ca785513c88eff6f4dea41
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-12-20 23:12:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,29 +53,16 @@ class ApplicationController < Sinatra::Base
     redirect '/'
   end
 
-  patch "/account" do
-
-
-
-    current_user = User.find(session[:user_id])
-    # current_user.balance = current_user.balance + deposit(params[:deposit])
-    current_user.update(params[:balance])
-    redirect '/account'
-
-    # redirect "/articles/#{article.id}"
-    # user = User.find(params[:id])
-    # puts user.name
-      # balance = deposit(params[:deposit].to_d)
-      # puts balance
-    #   # redirect '/account'
+  # still working on getting the balance to update for a user after a deposit or withdrawal
+  post "/deposit" do
+    blah = current_user.balance + params[:deposit].to_d
+    # puts blah
+    # User.update_attribute(current_user.id, :balance => blah)
+    puts current_user.update(username: params[:username], password: params[:password], balance: blah)
+    # current_user.save
+    # puts current_user.balance
   end
 
-  # post "/deposit" do
-  #   current_user.balance = deposit(params[:deposit].to_d)
-  #   puts current_user.balance
-  #   # redirect '/account'
-  #  end
-  #
   # post "/withdrawal" do
   #   withdrawal(params[:withdrawal].to_d)
   #   # redirect '/account'
@@ -91,8 +78,9 @@ class ApplicationController < Sinatra::Base
     end
 
     def deposit(amount)
-      balance = current_user.balance + amount
-      sprintf("%.2f", balance)
+      new_balance = current_user.balance + amount.to_d
+      current_user.update(balance: new_balance)
+      # sprintf("%.2f", balance)
     end
 
     def withdrawal(amount)
@@ -107,8 +95,6 @@ class ApplicationController < Sinatra::Base
     # def current_balance
     #   # balance = current_user.balance + deposit - withdrawal
     #   # sprintf("%.2f", balance)
-    #
-    #
     # end
   end
 
