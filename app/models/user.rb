a62b76fb2515d71e2e09ce34201f5f57256daaf3
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-12-31 00:33:36 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
+  def inititalize(balance=0)
+    @balance = balance
+  end
+  
+  def display_balance
+      @balance
+  end
+  
+  def deposit(amount)
+      @balance += amount
+  end
+  
+  def withdraw(amount)
+    if !amount > @balance
+      @balance -= amount
+    else
+      raise.error 
+    end
+  end
 end
