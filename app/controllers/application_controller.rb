86ffb55e4aa95709c6f1428278e1eb8c804f9021
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2019-01-22 20:30:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,6 @@ class ApplicationController < Sinatra::Base
     erb :account
   end
 
-
   get "/login" do
     erb :login
   end
---
+++ db/migrate/02_rename_password_column_to_password_digest.rb
@@ -0,0 +1,5 @@
+class RenamePasswordColumnToPasswordDigest < ActiveRecord::Migration
+  def change
+      rename_column :users, :password, :password_digest
+  end
+end
