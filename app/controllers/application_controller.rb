1e1c899fc597ff676123a527bac6f88e162cb4e3
brad schrag <brad.schrag@gmail.com>
2019-01-23 00:23:48 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:username => params[:username], :password_digest => params[:password])
-
+    
     if user.save
       redirect '/login'
     else
