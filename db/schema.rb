26dbfc628729135aa58a53c557e78f57930f8fa8
brandong1 <brandon@brandonleeproductions.com>
2019-01-24 19:53:19 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 20190111070133) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
