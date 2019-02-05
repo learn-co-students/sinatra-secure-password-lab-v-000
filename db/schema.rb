c9f4e0dd6fd16b7c53228096af0af32b28836073
Christiana Vandermale <christianavdm@gmail.com>
2019-02-05 15:11:51 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181108) do
+ActiveRecord::Schema.define(version: 20190205) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
+    t.string "password_digest"
   end
 
 end
