9d4a771979982e129b1092d83162c198d19e47e0
github username <github email address>
2019-02-09 20:23:42 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
-  end
+		user = User.new(:username => params[:username], :password => params[:password])
+    if params[:username] == ""
+      redirect "/failure"
+    elsif user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
+	end
 
   get '/account' do
     @user = User.find(session[:user_id])
@@ -32,8 +38,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
-  end
+		user = User.find_by(:username => params[:username])
+
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+      redirect "/account"    
+		else
+			redirect "/failure"
+		end
+	end
 
   get "/failure" do
     erb :failure
