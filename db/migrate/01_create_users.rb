e3da759dab20c0a97edcf900f08157a11a3ff472
Ashley Wong <ashwong04@gmail.com>
2019-03-04 22:38:03 +0000

    Done.

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |u|
       u.string :username
-      u.string :password
+      u.string :password_digest
     end
   end
   
