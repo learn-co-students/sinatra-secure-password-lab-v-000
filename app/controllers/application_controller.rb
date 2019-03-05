b9f68bc64ce882bac6140474f737580433a9c08a
Ashley Wong <ashwong04@gmail.com>
2019-03-05 04:12:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     @user = User.new(:username => params[:username],:password => params["password"])
-    if @user.save
+    if @user.save && !@user.username.empty?
       redirect '/account'
     else
       redirect '/failure'
@@ -39,9 +39,9 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect '/account'
+      redirect "/account"
     else
-      redirect '/failure'
+      redirect "/failure"
     end
   end
   
