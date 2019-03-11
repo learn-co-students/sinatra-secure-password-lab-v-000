2f8725d5647c7dc93b56a619e67e03179099936e
Katie Moosman <moosman.katie@gmail.com>
2019-03-11 21:17:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(:username => params[:username])
-    binding.pry
+    # binding.pry
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect to "/account"
--- db/migrate/001_create_users_table.rb
+++ db/migrate/001_create_users_table.rb
@@ -1,4 +1,4 @@
-class CreateUsersTable < ActiveRecord::Migration
+class CreateUsersTable < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :username
