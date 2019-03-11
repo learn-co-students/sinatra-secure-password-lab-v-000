7bc5cf6b38ed8313699e27ef2ca424093f5fdfea
Natalie Halawani <hala1224@gmail.com>
2019-03-11 03:14:15 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+# class User < ActiveRecord::Base
+  
+#   has_secure_password
+# end
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  has_secure_password
+
+  # my_password = BCrypt::Password.create("my password")
 end
