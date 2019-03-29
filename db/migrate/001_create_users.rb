50bd04f2dbfe2bde6df2e9ebd150f2ebc6574009
Cheryl K <cherylkelson@icloud.com>
2019-03-29 14:14:50 +0000

    Done.

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,9 +1,9 @@
-class User <ActiveRecord: Migration
+class CreateUsers <ActiveRecord::Migration
 
   def change
     create_table :users do |t|
     t.string :username
-    t.string :password
+    t.string :password_digest
     end
   end
 end
