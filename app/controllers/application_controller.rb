7988b870263a44e7578c9fbc67004b309a1503ad
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2019-07-16 18:36:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 require "./config/environment"
 require "./app/models/user"
-class ApplicationController < Sinatra::Base
+require 'pry'
+class ApplicationController < Sinatra::Base[4.7]
 
   configure do
     set :views, "app/views"
@@ -17,8 +18,16 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
+    if params[:username].empty?
+      redirect to '/failure'
+    end
 
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect '/login'
+    else
+      redirect '/failure'
+    end
   end
 
   get '/account' do
@@ -32,7 +41,22 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/account'
+    else
+      redirect to '/failure'
+    end
+  end
+
+  get "/success" do
+    if logged_in?
+      erb :success
+    else
+      redirect "/login"
+    end
   end
 
   get "/failure" do
