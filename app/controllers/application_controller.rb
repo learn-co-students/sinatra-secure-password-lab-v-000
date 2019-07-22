bfe7aa3936bf38e899e5000276e6a8e48ef4039f
Karly Mareka <karly.mareka@gmail.com>
2019-07-22 22:37:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require "./config/environment"
 require "./app/models/user"
 class ApplicationController < Sinatra::Base
@@ -17,7 +18,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
+    user = User.new(:username => params[:username], :password => params[:password])
+		if user.save && user.username.length != 0   
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
 
   end
 
@@ -32,7 +38,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username])
+		  if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/account"
+      else
+        redirect "/failure"
+      end
   end
 
   get "/failure" do
